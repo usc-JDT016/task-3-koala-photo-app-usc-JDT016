d303d48208ddc981832f991f222725ce884e556e
usc-JDT016 <JDT016@student.usc.edu.au>
2022-10-21 10:12:41 +1000

    test

---
+++ app/src/main/java/com/bignerdranch/android/csc202_assessmen3_koalaphotoapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.csc202_assessmen3_koalaphotoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
